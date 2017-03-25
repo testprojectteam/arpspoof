2521e540f26ceb76403ae6b018027c7d7a53012f
simran <aggarwalsimran103@gmail.com>
2017-03-25 23:24:04 +0530

    Sniffer Updated

--- arpsniff.py
+++ arpsniff.py
@@ -7,9 +7,10 @@ def arp_frame(data):
  arpHeaderUnpacked = struct.unpack('!HHBBH6s4s6s4s', arpHeader)
  arpSenderHardAddress = arpHeaderUnpacked[5]
  arpTargetHardAddress = arpHeaderUnpacked[7]
+ arpTargetProtAddress = socket.inet_ntoa(arpHeaderUnpacked[8])
  arpSenderHardAddress = '%.2x:%.2x:%.2x:%.2x:%.2x:%.2x' % (ord(arpSenderHardAddress[0]), ord(arpSenderHardAddress[1]), ord(arpSenderHardAddress[2]), ord(arpSenderHardAddress[3]), ord(arpSenderHardAddress[4]), ord(arpSenderHardAddress[5]))
  arpTargetHardAddress = '%.2x:%.2x:%.2x:%.2x:%.2x:%.2x' % (ord(arpTargetHardAddress[0]), ord(arpTargetHardAddress[1]), ord(arpTargetHardAddress[2]), ord(arpTargetHardAddress[3]), ord(arpTargetHardAddress[4]), ord(arpTargetHardAddress[5]))
- return arpTargetHardAddress,arpSenderHardAddress
+ return arpTargetHardAddress,arpSenderHardAddress,arpTargetProtAddress
 
 
 # Return properly formatted MAC address (ie AA:BB:CC:DD:EE:FF)
@@ -29,9 +30,10 @@ def main():
  while True:
   raw_data, addr = conn.recvfrom(65535)
   ethdest_mac, ethsrc_mac, eth_proto, data = ethernet_frame(raw_data)[0], ethernet_frame(raw_data)[1], ethernet_frame(raw_data)[2], ethernet_frame(raw_data)[3]
-  arpdest_mac, arpsrc_mac = arp_frame(raw_data)[0], arp_frame(raw_data)[1]
+  arpdest_mac, arpsrc_mac, dest_ip = arp_frame(raw_data)[0], arp_frame(raw_data)[1], arp_frame(raw_data)[2]
   if(ethdest_mac==arpdest_mac):
    print ('\nEthernet Frame:')
+   print dest_ip
    print ('Destination: {}, Source: {}, Protocol: {}'.format(ethdest_mac, ethsrc_mac, eth_proto))
 
    
