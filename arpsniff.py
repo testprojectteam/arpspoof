7167572a244147ff67bbaaf13e761cd2f9bdec5f
Meeta <meeta.1995@gmail.com>
2017-03-26 17:18:52 +0530

    Add files via upload

--- arpsniff.py
+++ arpsniff.py
@@ -7,10 +7,12 @@ def arp_frame(data):
  arpHeaderUnpacked = struct.unpack('!HHBBH6s4s6s4s', arpHeader)
  arpSenderHardAddress = arpHeaderUnpacked[5]
  arpTargetHardAddress = arpHeaderUnpacked[7]
- arpTargetProtAddress = socket.inet_ntoa(arpHeaderUnpacked[8])
+ arpTargetProtAddress = socket.inet_ntoa(arpHeaderUnpacked[6])
  arpSenderHardAddress = '%.2x:%.2x:%.2x:%.2x:%.2x:%.2x' % (ord(arpSenderHardAddress[0]), ord(arpSenderHardAddress[1]), ord(arpSenderHardAddress[2]), ord(arpSenderHardAddress[3]), ord(arpSenderHardAddress[4]), ord(arpSenderHardAddress[5]))
  arpTargetHardAddress = '%.2x:%.2x:%.2x:%.2x:%.2x:%.2x' % (ord(arpTargetHardAddress[0]), ord(arpTargetHardAddress[1]), ord(arpTargetHardAddress[2]), ord(arpTargetHardAddress[3]), ord(arpTargetHardAddress[4]), ord(arpTargetHardAddress[5]))
- return arpTargetHardAddress,arpSenderHardAddress,arpTargetProtAddress
+ arpoperation= arpHeaderUnpacked[4]
+ #arpoperation= '%.2x %.2x' % (ord(arpoperation[0]),ord(arpoperation[1]))
+ return arpTargetHardAddress,arpSenderHardAddress,arpTargetProtAddress, arpoperation
 
 
 # Return properly formatted MAC address (ie AA:BB:CC:DD:EE:FF)
@@ -30,10 +32,11 @@ def main():
  while True:
   raw_data, addr = conn.recvfrom(65535)
   ethdest_mac, ethsrc_mac, eth_proto, data = ethernet_frame(raw_data)[0], ethernet_frame(raw_data)[1], ethernet_frame(raw_data)[2], ethernet_frame(raw_data)[3]
-  arpdest_mac, arpsrc_mac, dest_ip = arp_frame(raw_data)[0], arp_frame(raw_data)[1], arp_frame(raw_data)[2]
+  arpdest_mac, arpsrc_mac, dest_ip, arpop = arp_frame(raw_data)[0], arp_frame(raw_data)[1], arp_frame(raw_data)[2],  arp_frame(raw_data)[3]
   if(ethdest_mac==arpdest_mac):
    print ('\nEthernet Frame:')
    print dest_ip
+   print arpop
    print ('Destination: {}, Source: {}, Protocol: {}'.format(ethdest_mac, ethsrc_mac, eth_proto))
 
    
